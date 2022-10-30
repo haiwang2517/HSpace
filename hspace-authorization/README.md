738fd07be30140ecde58c04cedf67e34ada5176c
8535 <yan.li02@hand-china.com>
2022-10-30 15:24:02 +0800

    搭建授权服务代码仓库

---
+++ hspace-authorization/README.md
@@ -0,0 +1,5 @@
+* token 颁发
+* token 刷新
+* token 验证
+
+基于oauth2.0 + JWT 实现授权服务。
---
+++ hspace-authorization/src/main/java/com/ken/hspace/authentication/Main.java
@@ -0,0 +1,11 @@
+package com.ken.hspace.authentication;
+
+/**
+ * @author haiyinlong
+ * @date 2022-10-30
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
